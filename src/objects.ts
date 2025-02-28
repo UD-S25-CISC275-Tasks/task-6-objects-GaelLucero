6f27d3fa9cbf364ad938e1462a24c9dff2a1df01
Gael Lucero <gael@udel.edu>
2025-02-27 22:57:49 -0500

    Completed ninth function

--- src/objects.ts
+++ src/objects.ts
@@ -116,7 +116,8 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    let copyQuestion = {...question, options: [...question.options, newOption]}
+    return copyQuestion;
 }
 
 /**
